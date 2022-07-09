8a66dfedf67d03cd9eb46741cd724c0dd98524c6
Braian Hercun <94329348+HercunBraian@users.noreply.github.com>
2022-07-09 17:29:36 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let nombre = "pepe";
+let edad = 25;
+let precio = 99.90;
+let seriesFavoritas = ["Dark", "Mr. Robot", "Castlevania"];
+let peliculasFavoritas = [
+        [nombre = "En busqueda de la felicidad",
+         año = 1994,
+         protagonista = "Braian"        
+        ],
+        [nombre = "El Rey Leon",
+        año = 1994,
+        protagonista = "Mufasa"        
+       ],
+       [nombre = "300",
+       año = 2008,
+       protagonista = "Leonidas"        
+      ]
+]
+
+
+console.log(nombre)
+console.log(edad)
+console.log(precio)
+console.log(seriesFavoritas)
+console.log(peliculasFavoritas)
+
+edad = 25 + 1
+
+console.log(edad)
+
+seriesFavoritas.push(nombre = "Jumanji",
+año = 1994,
+protagonista = "Juma"        
+)
+
+console.log(seriesFavoritas)
