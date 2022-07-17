9bac44120ef9816acfaf0683057aa1da4138029f
Braian Hercun <94329348+HercunBraian@users.noreply.github.com>
2022-07-17 20:05:58 -0300

    Primer desafio Class

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
-let nombre = "pepe";
-let edad = 25;
-let precio = 99.90;
-let seriesFavoritas = ["Dark", "Mr. Robot", "Castlevania"];
-let peliculasFavoritas = [
-        [nombre = "En busqueda de la felicidad",
-         año = 1994,
-         protagonista = "Braian"        
-        ],
-        [nombre = "El Rey Leon",
-        año = 1994,
-        protagonista = "Mufasa"        
-       ],
-       [nombre = "300",
-       año = 2008,
-       protagonista = "Leonidas"        
-      ]
-]
+class Usuario {
+      constructor(nombre, apellido, libros, mascotas) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+        this.nombresADevolver = [];
+        this.mascotasADevolver = [];
+      }
+    
+      getFullName() {
+        return console.log(
+          `El empleado se llama ${this.nombre} ${this.apellido}`
+        );
+      }
+      addMascota(nombreMascota) {
+        this.mascotas.push(nombreMascota);
+      }
+      countMascotas() {
+        return console.log(`La cantidad de mascotas es ${this.mascotas.length}`);
+      }
+      addBook(libro) {
+        this.libros.push(libro);
+      }
+      getBookNames() {
+        this.libros.forEach((libro) => {
+          this.nombresADevolver.push(libro.nombre);
+        });
+    
+        console.log(this.nombresADevolver);
+      }
 
+      getMascotasNames() {
+        this.mascotas.forEach((mascota) => {
+          this.mascotasADevolver.push(mascota);
+        });
+    
+        console.log(this.mascotasADevolver);
+      }
+    }
+    
+    let newUser = new Usuario(
+      'Braian',
+      'Hercun',
+      [
+        { nombre: 'Los ojos del perro siberiano', Autor: 'Antonio Santa Ana' },
+        { nombre: 'Padre rico, padre pobre', Autor: 'Robert Kiyosaki' },
+      ],
+      ['Perro', 'Conejo']
+    );
+    
+    newUser.getFullName();
+    
+    newUser.addMascota('Gato');
+    
+    newUser.countMascotas();
+    
+    newUser.addBook({ nombre: 'Perros de nadie', Autor: 'Esteban Valentino' });
+    
+    newUser.getBookNames();
 
-console.log(nombre)
-console.log(edad)
-console.log(precio)
-console.log(seriesFavoritas)
-console.log(peliculasFavoritas)
-
-edad = 25 + 1
-
+    newUser.getMascotasNames();
+    
